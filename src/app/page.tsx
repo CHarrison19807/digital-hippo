b5b87dae4c0355939a510e552dd152f93ec88a45
Connor Harrison <connorh.uottawa@gmail.com>
2024-01-28 00:37:26 -0500

    feat: create ProductReel and children components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import ProductReel from "@/components/ProductReel";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
 import Link from "next/link";
@@ -45,7 +46,12 @@ export default function Home() {
             <Button variant="ghost">Our quality products &rarr;</Button>
           </div>
         </div>
-        {/* TODO: Add a grid of products here */}
+        <ProductReel
+          query={{ sort: "desc", limit: 4 }}
+          href="/products"
+          title="brand new"
+          subtitle="subtitle"
+        />
       </MaxWidthWrapper>
       <section className="border-t border-gray-200 bg-gray-50">
         <MaxWidthWrapper className="py-20">
