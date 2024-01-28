3c2e8f66a8f0c8e51fee7439ac74ab92fead379c
Connor Harrison <connorh.uottawa@gmail.com>
2024-01-28 18:57:14 -0500

    fix: typo in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
           </p>
 
           <div className="flex flex-col sm:flex-row gap-4 mt-6">
-            <Link href="/profucts" className={buttonVariants()}>
+            <Link href="/products" className={buttonVariants()}>
               Browse Trending
             </Link>
             <Button variant="ghost">Our quality products &rarr;</Button>
